ab95462f4b1d099292aa5e6cc43f58ab28598795
Bruno Camino <camino.bruno@gmail.com>
2021-12-08 16:08:43 +0000

    Add newk to cry_properties.py

--- cry_properties.py
+++ cry_properties.py
@@ -6,3 +6,13 @@ Created on Wed Dec  8 15:35:48 2021
 @author: brunocamino
 """
 
+def newk(shrink1,shrink2,Fermi=1,print_option=0):
+    
+    newk_block = ['NEWK\n','%s %s\n' %(shrink1,shrink2),
+                  '%s %s\n'%(Fermi,print_option)]
+    
+    return newk_block
+    
+def bands(k_path,n_kpoints,first_band,last_band,print_eig=0,print_option=1):
+    pass
+    
