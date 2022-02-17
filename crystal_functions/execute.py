f1134d3400fe92cd1d85b3265ca1ccee888d524d
Bruno Camino <camino.bruno@gmail.com>
2022-02-17 17:51:28 +0000

    Add set_runcry_path

--- crystal_functions/execute.py
+++ crystal_functions/execute.py
@@ -6,10 +6,56 @@ Created on Fri Nov 19 18:28:54 2021
 @author: brunocamino
 """
 
-def runcry(file_name,guessp=None):
-    runcry_path = '/Users/brunocamino/crystal/runcry17'
-    if runcry_path == None:
-        return('Please set the runcry path before calling this function')
+def set_runcry_path(path):
+
+    import re
+    import os
+
+    this_file = os.path.realpath(__file__)
+    # Read the current settings file
+    file = open(this_file, 'r')
+    lines = file.readlines()
+    file.close()
+
+    for i, line in enumerate(lines):
+        if re.match(r'    runcry_path', line) != None:
+            if len(path) > 0:
+                lines[i] = '    runcry_path = \'%s\'\n' % (path)
+
+    # Write the newly defined variables to the settings file
+    file = open(this_file, 'w')
+    for line in lines:
+        file.writelines(line)
+    file.close()
+
+def set_runprop_path(path):
+
+    import re
+    import os
+
+    this_file = os.path.realpath(__file__)
+    # Read the current settings file
+    file = open(this_file, 'r')
+    lines = file.readlines()
+    file.close()
+
+    for i, line in enumerate(lines):
+        if re.match(r'    runprop_path', line) != None:
+            if len(path) > 0:
+                lines[i] = '    runprop_path = \'%s\'\n' % (path)
+
+    # Write the newly defined variables to the settings file
+    file = open(this_file, 'w')
+    for line in lines:
+        file.writelines(line)
+    file.close()
+
+
+def runcry(file_name, guessp=None):
+
+    runcry_path = None
+    if runcry_path is None:
+        return 'Please set the runcry path before calling this function'
 
     import subprocess
     import sys
@@ -18,28 +64,28 @@ def runcry(file_name,guessp=None):
     converged = False
     index = 0
 
-    #file_name = file_name.split('.')[0]
+    # file_name = file_name.split('.')[0]
 
-    while converged == False:
+    if converged is False:
         if index > 3:
             return None
         else:
-            if guessp != None:
+            if guessp is not None:
                 run_calc = runcry_path + ' ' + file_name + ' ' + guessp
             else:
                 run_calc = runcry_path + ' ' + file_name
-            process = subprocess.Popen(run_calc.split(), stdout=subprocess.PIPE, stderr= subprocess.PIPE)
+            process = subprocess.Popen(run_calc.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output, error = process.communicate()
             index += 1
             try:
-                file = open('%s.out'%(file_name), 'r')
+                file = open('%s.out' % file_name, 'r')
                 data = file.readlines()
                 file.close()
             except:
                 print('EXITING: a .out file needs to be specified')
                 sys.exit(1)
-            for i,line in enumerate(data[::-1]):
-                if re.match(r'^ EEEEEEEEEE TERMINATION',line) != None:
+            for i, line in enumerate(data[::-1]):
+                if re.match(r'^ EEEEEEEEEE TERMINATION', line) is not None:
                     converged = True
                     return '%s.out calculation successfully completed' % file_name
 
@@ -47,8 +93,8 @@ def runcry(file_name,guessp=None):
 
 def runprop(prop_name,wf_file):
 
-    runprop_path = '/Users/brunocamino/crystal/runprop17'
-    if runprop_path == None:
+    runprop_path = None
+    if runprop_path is None:
         return('Please set the runprop path before calling it')
 
     import subprocess
@@ -76,6 +122,5 @@ def runprop(prop_name,wf_file):
             if re.match(r'^ EEEEEEEEEE TERMINATION',line) != None:
                 converged = True
                 return '%s.outp calculation successfully completed' % prop_name
-                break
 
     return None
