c6f5fb0058fae9ed8b93ebcd02850967bad7c7dc
Bruno Camino <camino.bruno@gmail.com>
2022-03-29 13:58:04 +0100

    Add plot.py

--- crystal_functions/plot.py
+++ crystal_functions/plot.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 19 18:27:27 2021
+Created on 29/03/2022
 
 @author: brunocamino
 """
+
+def plot_cry_bands():
+    pass
+
+def plot_cry_doss():
+    pass
+
+def plot_cry_es():
+    pass
