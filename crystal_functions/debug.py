156da75ee2dff4a4c960ae97a18532cd7c2481ac
Alessandro Cossard <alessandro.cossard@unito.it>
2022-09-05 19:10:40 +0200

    cominciato il debug

---
+++ crystal_functions/debug.py
@@ -0,0 +1,11 @@
+print('hello')
+
+from file_readwrite import Properties_output
+
+print('ho importato')
+
+urea_contour_output = Properties_output()
+
+print('ho istanziato')
+
+#DA QUI SI COMINCIA
