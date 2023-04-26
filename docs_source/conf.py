073a361edecea8e89c9eb275cf5a95fe8f449de2
Huanyu Zhou <61191919+Spica-Vir@users.noreply.github.com>
2023-04-26 14:39:23 +0100

    doc site debug

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -37,8 +37,8 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary",  
-			  "sphinx.ext.napoleon"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',  
+			  'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
 # Set `Returns` section to behave like the `Args` section
 # For Google Doc format
 napoleon_custom_sections = [('Returns', 'params_style')]
@@ -96,6 +96,9 @@ html_theme_path = ['.']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The URL to doc site
+html_baseurl = 'https://crystal-code-tools.github.io/CRYSTALpytools/'
 
 # -- Options for LaTeX intepreter---------------------------------------------
 latex_elements = {
