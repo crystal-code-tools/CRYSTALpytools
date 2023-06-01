76353f8ca4d7f2785350750033110c1167606cae
Spica-Vir <spica.h.zhou@gmail.com>
2023-06-01 20:55:24 +0100

    Fix the link to Github on doc site

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -82,7 +82,7 @@ html_context = {
     'display_github'  : True,
     'github_user'     : 'crystal-code-tools',
     'github_repo'     : 'CRYSTALpytools',
-    'github_version'  : 'master',
+    'github_version'  : 'main',
     'conf_py_path'    : '/docs_source/',
 }
 
