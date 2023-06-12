67ff6c9e4d5c1b8451123692a2957d85ab05de68
Spica-Vir <spica.h.zhou@gmail.com>
2023-06-12 17:17:40 +0100

    frequency output base object

--- CRYSTALpytools/base/__init__.py
+++ CRYSTALpytools/base/__init__.py
@@ -1,3 +1,4 @@
 from CRYSTALpytools.base import basisset
-from CRYSTALpytools.base import crysd12
 from CRYSTALpytools.base import inputbase
+from CRYSTALpytools.base import crysd12
+from CRYSTALpytools.base import crysout
