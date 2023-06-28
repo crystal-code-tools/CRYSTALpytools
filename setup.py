ea5653a2a6d8a686f1c1d6f63277da67a8e6de24
Huanyu Zhou <61191919+Spica-Vir@users.noreply.github.com>
2023-06-28 11:50:59 +0100

    Requirement bugfix

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
+from importlib.metadata import version
 
 long_description = 'Python tools for the <a href="https://www.crystal.unito.it/index.php">CRYSTAL code</a> developed and mantained by the CRYSTAL code developers'
 
 setuptools.setup(
     name="CRYSTALpytools",        
-    version="2023.04.04",
+    version=version('CRYSTALpytools'),
     author_email="crystalcodetools@gmail.com",
     description="Python tools for the CRYSTAL code developed and mantained by the CRYSTAL code developers.",
     long_description=long_description,
@@ -21,8 +22,15 @@ setuptools.setup(
     packages=setuptools.find_packages(include=['CRYSTALpytools', 'CRYSTALpytools.*']),
     #python_requires=">=3.8",
     install_requires=[
-    "pymatgen",
-    "mendeleev",
-    "ase"
+	"numpy",
+	"sympy",
+	"scipy",
+	"matplotlib",
+	"pandas",
+	"PyYAML",
+	"mendeleev>=0.14.0",
+    "pymatgen>=2022.7.25",
+    "ase>=3.22.1",
+    "basis_set_exchange>=0.9.1"
     ]
 )
